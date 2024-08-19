5f199e1271f1e23202f1e72c49027fbe2cabb416
Yana <yana102hanan@gmail.com>
2024-08-19 14:33:03 +0300

    Add sidebar

--- src/pages/Workspace.jsx
+++ src/pages/Workspace.jsx
@@ -1,24 +1,20 @@
 import { useNavigate } from "react-router";
 import { useSelector } from "react-redux";
 
+import { Sidebar } from "../cmps/Sidebar";
+
 export function Workspace() {
   const navigate = useNavigate();
 
-  const user = useSelector((state) => state.userModule.loggedInUser);
+  // const user = useSelector((state) => state.userModule.loggedInUser);
 
   return (
     <section className="workspace">
-      {user && (
-        <>
-          <h1>
-            welcome <span>{user.fullname}</span>
-          </h1>
-          <h2>Your Boards</h2>
-          <div className="new-board" onClick={() => navigate("/board/123")}>
-            <span>Create new board</span>
-          </div>
-        </>
-      )}
+      <Sidebar />
+      <h2>Your Boards</h2>
+      <div className="new-board" onClick={() => navigate("/board/123")}>
+        <span>Create new board</span>
+      </div>
     </section>
   );
 }
